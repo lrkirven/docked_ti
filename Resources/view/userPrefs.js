17f0f3038696d24cabba47d924bc946ef3621c72
Lazar Kirven <lrkirven@gmail.com>
2011-01-31 22:52:31 -0600

    initial coding

---
+++ Resources/view/userPrefs.js
@@ -0,0 +1,155 @@
+Ti.include('../util/tools.js');
+Ti.include('../model/modelLocator.js');
+Ti.include('../client/picasaClient.js');
+Ti.include('../client/restClient.js');
+Ti.include('../util/tea.js');
+
+var win = Ti.UI.currentWindow;
+var model = win.model;
+var db = win.db;
+var css = win.css;
+var saveBtn = null;
+var displayNameText = null;
+
+Titanium.App.addEventListener('UPDATED_DISPLAY_NAME', function(e) { 
+	if (e.status == 0) {
+		Tools.reportMsg(model.getAppName(), 'User preferences updated!');	
+	}
+	else {
+		Tools.reportMsg(model.getAppName(), e.errorMsg);
+	}
+});
+
+/**
+ * This method lays out the UI format and sets up the event listeners to 
+ * handle user interaction.
+ */
+function buildForm() {
+
+	var panel = Ti.UI.createView({ 
+		backgroundColor:'#cccccc',
+		top:10,
+		bottom:10,
+		left:10,
+		width:300,
+		borderRadius:20,
+		clickName:'bg'
+	});
+
+	var defaultIDImage = Ti.UI.createImageView({
+		image: '../user.png',
+		backgroundColor:css.getColor0(),
+		borderColor:css.getColor2(),
+		top:10,
+		left:20,
+		width:50,
+		height:50,
+		clickName:'defaultIDImage'
+	});
+	panel.add(defaultIDImage);
+	
+	var appName = Titanium.UI.createLabel({
+		color: css.getColor0(),
+		text: model.getAppName(),
+		font: { fontFamily: model.myFont, fontSize:25, fontWeight: 'bold' },
+		top: 10,
+		right: 20,
+		width: 280,
+		textAlign: 'right',
+		shadowColor:'#eee',
+		shadowOffset:{x:0,y:1},
+		height: 'auto'
+	});
+	panel.add(appName);
+
+	var lbl = Titanium.UI.createLabel({
+		color: css.getColor0(),
+		text: 'Display name to \'Docked\' Community: ',
+		font: { fontFamily: model.myFont, fontSize:15 },
+		top: 80,
+		left: 10,
+		width: 280,
+		textAlign: 'left',
+		height: 'auto'
+	});
+	panel.add(lbl);
+
+	displayNameText = Titanium.UI.createTextField({
+		height: 40,
+		width: 280,
+		left: 10,
+		top: 105,
+		font: { fontFamily: model.myFont, fontWeight: 'normal' },
+		textAlign: 'left',
+		keyboardType: Titanium.UI.KEYBOARD_NUMBERS_PUNCTUATION,
+		borderStyle: Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+		borderWidth: 2,
+		borderRadius: 1
+	});	
+	displayNameText.addEventListener('change', function() {
+		var name = displayNameText.value;
+		if (name != null && name.length > 0) {
+			saveBtn.enabled = true;	
+			if (name.length > 50) {
+				name = name.substr(0, 50);
+				displayNameText.value = name;
+			}
+		}
+		else {
+			saveBtn.enabled = false;
+		}
+	});
+	panel.add(displayNameText);
+	
+	//
+	// submit button
+	//
+	saveBtn = Titanium.UI.createButton({
+		title: 'Save',
+		enabled: true,
+		color: css.getColor0(),
+		systemButton: Titanium.UI.iPhone.SystemButton.FLEXIBLE_SPACE,
+		selectedColor:css.getColor2(),
+		bottom: 10,
+		borderRadius:0,
+		right: 20,
+		height: 30,
+		width: 100
+	});
+	saveBtn.addEventListener('click', function() {
+		var str = displayNameText.value;	
+		if (str != model.getCurrentUser().displayName) {
+			var client = new RestClient();
+			var user = model.getCurrentUser();
+			client.updateDisplayName(user.id, str);	
+		}
+	});
+	
+	if (model.getCurrentUser() != null && model.getCurrentUser().displayName != null) {
+		displayNameText.value = model.getCurrentUser().displayName;
+		displayNameText.focus();
+		saveBtn.enabled = true;	
+	}
+	else {
+		saveBtn.enabled = false;	
+	}
+	panel.add(saveBtn);
+	win.add(panel);
+	win.backgroundImage = '../dockedbg.png';
+};
+
+
+/**
+ * Initialize the form
+ */
+function init() {
+	win.addEventListener("open", function(event, type) {
+    	displayNameText.focus();
+	});
+	buildForm();
+};
+
+//
+// initial entry
+//
+init();
