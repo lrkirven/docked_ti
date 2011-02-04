eaeb90603a298dc1641b11e04e5b2b267ec29776
Lazar Kirven <lrkirven@gmail.com>
2011-02-04 17:05:39 -0600

    added few generic messages ...

--- Resources/util/msgs.js
+++ Resources/util/msgs.js
@@ -3,4 +3,7 @@
 	
 	Msgs.OUT_OF_ZONE = '[OUTSIDE LAKE ZONE]';
 	Msgs.USERS = ' USER(S)';
+	Msgs.TAKE_PIC = 'Take a picture';
+	Msgs.SELECT_FROM_GALLERY = 'From gallery';
+	Msgs.MSG_POSTED = 'Message posted to Docked!';
 	
