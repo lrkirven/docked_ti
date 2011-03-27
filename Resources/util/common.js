e2eade33278ec4e5f8695fe83f658c7ed1cc8f4c
Lazar Kirven <lrkirven@gmail.com>
2011-03-27 13:10:53 -0500

    Release 1.0.1

--- Resources/util/common.js
+++ Resources/util/common.js
@@ -2,6 +2,8 @@
 	var Common = {}; // App namespace
 
 	Common.VERSION = 'v1';	
+	Common.CLIENT_VERSION = '1.0.1';	
+	Common.CLIENT_REL_DATE = '03/26/2011';
 	Common.DEBUG = false;	
 	Common.RESET_APP = false;	
 	Common.MAX_DISPLAY_NAME_LENGTH = 15;
